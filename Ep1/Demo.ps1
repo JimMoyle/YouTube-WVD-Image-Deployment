db30b5631ce4f3f9e0e9410181484a29c1a4ac35
Jim Moyle <github@jimmoyle.com>
2021-01-18 16:26:10 +0000

    Added comment

--- Ep1/Demo.ps1
+++ Ep1/Demo.ps1
@@ -1,32 +1,35 @@
-#Check we're comnnected to Azure
+# All files will be available in my associated GitHub repository
+# https://github.com/JimMoyle/YouTube-WVD-Image-Deployment
+
+# Check we're comnnected to Azure
 (Get-AzContext).Account.id
 
-#There variables are needed so we know which image to base things from
+# There variables are needed so we know which image to base things from
 $Location = 'uksouth'
 $PublisherName = 'MicrosoftWindowsDesktop'
 $Offer = 'office-365'
 $SkuMatchString = '*evd*'
 
-#Making sure we know the latest version of the image published by Microsoft
+# Making sure we know the latest version of the image published by Microsoft
 $sku = Get-AzVMImageSku -Location $Location -PublisherName $PublisherName -Offer $Offer | Where-Object { $_.Skus -like $SkuMatchString }
 $sku
 $sku | Get-AzVMImage | Sort-Object -Descending -Property Version | Select-Object -First 1
 
-#I've put this into a function for you to use (works for any image, but defaults are for WVD)
+# I've put this into a function for you to use (works for any image, but defaults are for WVD)
 . .\Get-AzureImageInfo.ps1
 
 Get-AzureImageInfo -Location 'uksouth'
 
-#Validate JSON
+# Validate JSON
 & packer validate Windows10ManagedImage.json
 
-#Do the build
+# Do the build
 
-#All you really need is the JSON and this packer build line
+# All you really need is the JSON and this packer build line
 & packer build Windows10ManagedImage.json
 
 
-#Need an image definition in the gallery for this to work
+# Need an image definition in the gallery for this to work
 $ParamNewAzGalleryImageDefinition = @{
     GalleryName       = 'PackerSIG'
     ResourceGroupName = 'Packer'
@@ -43,5 +46,5 @@ New-AzGalleryImageDefinition @ParamNewAzGalleryImageDefinition
 
 & packer validate Windows10SharedImageGallery.json
 
-#Do the build
+# Do the build
 & packer build Windows10SharedImageGallery.json

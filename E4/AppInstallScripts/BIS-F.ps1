8bd06c14b887876c88422670f305bba6e776afdd
Jim Moyle <jim.moyle@microsoft.com>
2021-02-08 18:18:06 +0000

    BISF started

---
+++ E4/AppInstallScripts/BIS-F.ps1
@@ -0,0 +1,16 @@
+$BuildDir = 'c:\CustomizerArtifacts'
+if (-not(Test-Path $BuildDir)) {
+    New-Item  -ItemType Directory $BuildDir
+}
+$allVersions = Get-BISF
+$mostRecent = $allVersions | Sort-Object -Descending -Property 'Version' | Select-Object -First 1 | Select-Object -ExpandProperty 'Version'
+$allOnVersion = $allVersions | Where-Object { $_.version -eq $mostRecent }
+$myVersion = $allOnVersion
+$fileName = Split-Path $myVersion.uri -Leaf
+$outFile = Join-Path 'c:\CustomizerArtifacts' $fileName
+if (-not(Test-Path $outFile)) {
+    Invoke-WebRequest $myVersion.uri -OutFile $outFile
+}
+Start-Process -FilePath msiexec.exe -Argument "/i $outFile /qn" -Wait
+Remove-Item $outFile
+Write-Host 'BISF Installed'

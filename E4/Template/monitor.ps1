972a0fdafbc577e942a7fc17798f8b91b24165d9
Jim Moyle <jim.moyle@microsoft.com>
2021-02-09 18:10:42 +0000

    File organisation

--- E4/Template/monitor.ps1
+++
@@ -1,8 +0,0 @@
-$imageResourceGroup = 'YTAzureImageBuilderRG'
-$imageTemplateName = 'MicrosoftWindowsDesktop.office-365.20h2-evd-o365pp'
-while ($true) {
-    Get-AzImageBuilderTemplate -ImageTemplateName  $imageTemplateName -ResourceGroupName $imageResourceGroup |
-         Select-Object LastRunStatusRunState, LastRunStatusRunSubState, ProvisioningState, ProvisioningErrorMessage, LastRunStatusMessage
-    Start-Sleep 5
-    Clear-Host
-}

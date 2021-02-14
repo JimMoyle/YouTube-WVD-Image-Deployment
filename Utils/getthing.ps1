a1458ec6406cff3f14182aeaf97b8681ea988eac
Jim Moyle <jim.moyle@microsoft.com>
2021-02-14 11:21:32 +0000

    getething

--- Utils/getthing.ps1
+++ Utils/getthing.ps1
@@ -1,5 +1,5 @@
     $regex = '.*PACKER OUT     azure-arm: \$password = \"(\w*)\"'
-    $text = Get-ChildItem -path c:\Windows\temp -File -Filter *.ps1 | Get-Content
+    $text = Get-ChildItem -path "c:\Windows\temp" -File -Filter *.ps1 | Get-Content
     Write-Output $text
     ($text | Select-String -Pattern $regex).Matches.Value -match $regex #| Out-Null
     Write-Output $Matches[1]

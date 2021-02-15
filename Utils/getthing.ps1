486671001de8da6df74b4b565bcc348b47c490bb
Jim Moyle <jim.moyle@microsoft.com>
2021-02-15 22:24:28 +0000

    utils update

--- Utils/getthing.ps1
+++ Utils/getthing.ps1
@@ -2,13 +2,17 @@ param(
     $path = 'C:\jimm'
 )
 
-foreach ($file in Get-ChildItem C:\jimm\customization.log -File) { $rx = '\$password = \".\w*\"'; (Get-Content $file | Select-String -Pattern $rx).ToString()}
+foreach ($file in Get-ChildItem C:\jimm\customization.log -File) { $rx = '\$password = \".\w*\"'; (Get-Content $file | Select-String -Pattern $rx).ToString() }
 <#
-$regex = '.*PACKER OUT     azure-arm: \$password = \"(\w*)\"'
-$files =  Get-ChildItem -path "c:\Windows\temp" -File -Filter *.ps1
-Write-Output $files
-$text = $files | Get-Content
-Write-Output $text
-($text | Select-String -Pattern $regex).Matches.Value -match $regex #| Out-Null
-Write-Output $Matches[1]
+{
+    "type": "PowerShell",
+    "runElevated": true,
+    "name": "get ps1",
+    "inline": [
+        "Write-Output 'getting powershell files'",
+        "gci -path c:\\Windows\\temp -File -Filter *.ps1",
+        "gci -path c:\\Windows\\temp -File -Filter *.ps1 | get-content",
+        "Write-Output 'Done powershell files'"
+    ]
+},
 #>

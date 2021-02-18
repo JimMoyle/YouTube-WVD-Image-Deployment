8258fef5646d4e07b7f045acd816404139d4ae75
Jim Moyle <jim.moyle@microsoft.com>
2021-02-18 00:03:09 +0000

    organisation

---
+++ E5/AppInstallScripts/Modules.ps1
@@ -0,0 +1,7 @@
+Write-Output 'PSGallery Module Install Started'
+New-Item  -ItemType Directory 'C:\CustomizerArtifacts'  | Out-Null
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted  | Out-Null
+Install-Module EverGreen
+Install-Module Az
+Write-Output 'PSGallery Module Install Finished'
